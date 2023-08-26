311a3114f4b5c8bb2d50fe15ff8538c4e926a373
aabbq <135847807+aabbq@users.noreply.github.com>
2023-08-26 20:54:18 +0800

    added bank module

---
+++ src/app/_models/bank.ts
@@ -0,0 +1,8 @@
+import { Status } from "../_helpers/enums/status";
+
+export class Bank {
+    id?: string;
+    name?: string;
+    description?: string;
+    status?: Status;
+}
---
+++ src/app/_services/bank.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@app/environments/environment';
+import { Bank } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class BankService {
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) { }
+
+    getAll() {
+        return this.http.get<Bank[]>(`${environment.apiUrl}/banks`);
+    }
+
+    create(bank: Bank) {
+        return this.http.post(`${environment.apiUrl}/banks`, bank);
+    }
+
+    getById(id: string) {
+        return this.http.get<Bank>(`${environment.apiUrl}/banks/${id}`);
+    }
+
+    update(id: string, params: any) {
+        return this.http.put(`${environment.apiUrl}/banks/${id}`, params)
+            .pipe(map(x => {
+                return x;
+            }));
+    }
+}
