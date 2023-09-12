d76db402f4c05273952f668efca06acbfd2bc8fe
aabbq <135847807+aabbq@users.noreply.github.com>
2023-09-12 12:25:20 +0800

    update order gcash, grab and amount

--- src/app/order/list.component.ts
+++ src/app/order/list.component.ts
@@ -38,7 +38,8 @@ export class ListComponent implements OnInit {
     dataSource: any;
     displayedColumns: string[] = [
         'id', 'transaction_date', 'or_number', 'ordered_to', 
-        'total_amount', 'payment_type', 'total_cash', 'credit_card_amount', 'order_type', 'action'];
+        'total_amount', 'payment_type', 'cash_amount', 'credit_card_amount', 'gcash_amount', 
+        'grab_amount', 'panda_amount', 'order_type', 'action'];
     @ViewChild(MatPaginator) paginator !:MatPaginator;
     @ViewChild(MatSort) sort !:MatSort;
     
@@ -95,10 +96,22 @@ export class ListComponent implements OnInit {
     }
 
     getTotalCash() {
-        return this.orders?.map(t => t.total_cash).reduce((acc: any, value) => acc + value, 0);    
+        return this.orders?.map(t => t.cash_amount).reduce((acc: any, value) => acc + value, 0);    
     }
 
     getTotalCC() {
         return this.orders?.map(t => t.credit_card_amount).reduce((acc: any, value) => acc + value, 0);    
+    }
+
+    getTotalGCash() {
+        return this.orders?.map(t => t.gcash_amount).reduce((acc: any, value) => acc + value, 0);    
+    }
+
+    getTotalGrab() {
+        return this.orders?.map(t => t.grab_amount).reduce((acc: any, value) => acc + value, 0);    
+    }
+
+    getTotalPanda() {
+        return this.orders?.map(t => t.panda_amount).reduce((acc: any, value) => acc + value, 0);    
     }
 }
