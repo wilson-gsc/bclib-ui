94e872bb51aab4d5eb6d1bcda779d2b3668994a7
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 10:40:13 +0800

    added some model

--- src/app/_models/student.ts
+++ src/app/_models/student.ts
@@ -2,6 +2,12 @@ import { Status } from "../_helpers/enums/status";
 
 export class Student {
     id?: string;
-    name?: string;
+    student_id?: string;
+    first_name?: string;
+    last_name?: string;
+    full_name?: string;
+    course?: string;
+    year_level?: string;
+    enrollment_date?:Date;
     status?: Status;
 }
