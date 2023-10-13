f24ef2535e13693267e1926f599b7b75c49b3d3e
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 10:44:58 +0800

    added course, borrow record and employee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,9 @@ const studentsRoutes = () => import('./student/students.routes').then(x => x.STU
 const categorysRoutes = () => import('./category/categorys.routes').then(x => x.CATEGORYS_ROUTES);
 const authorsRoutes = () => import('./author/authors.routes').then(x => x.AUTHORS_ROUTES);
 const publishersRoutes = () => import('./publisher/publishers.routes').then(x => x.PUBLISHERS_ROUTES);
+const courseRoutes = () => import('./course/course.routes').then(x => x.COURSE_ROUTES);
+const employeeRoutes = () => import('./employee/employee.routes').then(x => x.EMPLOYEE_ROUTES);
+const borrow_recordRoutes = () => import('./borrow_record/borrow_record.routes').then(x => x.BORROW_RECORD_ROUTES);
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [authGuard] },
@@ -22,10 +25,15 @@ export const APP_ROUTES: Routes = [
 
     { path: 'publishers', loadChildren: publishersRoutes, canActivate: [authGuard] },
 
+    { path: 'course', loadChildren: courseRoutes, canActivate: [authGuard] },
+
     { path: 'books', loadChildren: booksRoutes, canActivate: [authGuard] },
 
     { path: 'students', loadChildren: studentsRoutes, canActivate: [authGuard] },
 
+   { path: 'employee', loadChildren: employeeRoutes, canActivate: [authGuard] },
+
+   { path: 'borrow_record', loadChildren: borrow_recordRoutes, canActivate: [authGuard] },
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
