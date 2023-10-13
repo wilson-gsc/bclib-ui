892f307f2b21dcab1c734aacb078ae0bb842d22c
Alan C. Gabales <agabales69@gmail.com>
2023-10-13 11:01:47 +0800

    added  code, name, status,

---
+++ src/app/course/list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { first } from 'rxjs/operators';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Course} from '@app/_models';
+import { CourseService } from '@app/_services/course.service';
+import { TableUtil } from '@app/_helpers/table.util';
+
+@Component({ 
+    selector: 'publisher-list-component',
+    templateUrl: 'list.component.html',
+    styleUrls: ['course.component.css'],
+    standalone: true,
+    imports: [
+        RouterLink, NgFor, NgIf,
+        MatCardModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatTableModule, MatPaginatorModule, MatSortModule,
+        MatIconModule
+    ]
+})
+export class ListComponent implements OnInit {
+
+    course?: Course[];
+    dataSource: any;
+    displayedColumns: string[] = ['id','code', 'name', 'status', 'action'];
+    @ViewChild(MatPaginator) paginator !:MatPaginator;
+    @ViewChild(MatSort) sort !:MatSort;
+    
+    constructor(private courseService: CourseService) {}
+
+    ngOnInit() {
+        this.getCourse();
+    }
+
+    getCourse() {
+        this.courseService.getAll()
+            .pipe(first())
+            .subscribe(course => {
+                this.course = course;
+                this.dataSource = new MatTableDataSource<Course>(this.course);
+                this.dataSource.paginator=this.paginator;
+                this.dataSource.sort=this.sort;
+            });
+    }
+
+    filterChange(data: Event){
+        const value = (data.target as HTMLInputElement).value;
+        this.dataSource.filter = value;
+    }
+
+    exportTable() {
+        TableUtil.exportTableToExcel("course", "Course");
+    }
+}
