f1c10ddcf0c28fbf6a6448e001a94b308c5c13e7
Wilson Gayo <wilschoy78@gmail.com>
2023-12-15 12:06:12 +0800

    view book

--- src/app/book/books.routes.ts
+++ src/app/book/books.routes.ts
@@ -3,6 +3,7 @@ import { authGuard } from '../_helpers';
 
 import { BookComponent } from './list.component';
 import { AddEditComponent } from './add-edit.component';
+import { ViewComponent } from './view.component';
 import { ListGroupComponent } from './list-group.component';
 import { CategoryAddEditComponent } from '@app/category/add-edit.component';
 import { CategoryListComponent } from '@app/category/list.component';
@@ -13,6 +14,7 @@ export const BOOKS_ROUTES: Routes = [
     { path: 'groups', component: ListGroupComponent },
     { path: 'add', component: AddEditComponent },
     { path: 'editbook/:id', component: AddEditComponent },
+    { path: 'view/:id', component: ViewComponent },
     { path: 'groups', component: ListGroupComponent },
     { path: './', component: CategoryListComponent },
     { path: 'addcategory', component: CategoryAddEditComponent },
